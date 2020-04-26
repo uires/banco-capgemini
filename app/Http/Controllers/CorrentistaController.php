ed0a707d57b8e006a4523a8e9228c4f8fe46f05b
uíres <uiresdeivide1@gmail.com>
2020-04-26 20:12:41 -0300

    Ajustando rotas e ajustando controlador correntista

--- app/Http/Controllers/CorrentistaController.php
+++ app/Http/Controllers/CorrentistaController.php
@@ -11,23 +11,10 @@ use App\ContaCorrente;
 
 class CorrentistaController extends Controller
 {   
-    /*
-        @despction: buscar todos os correntistas
-        @author: uíres
-        @routePath: /correntistas
-    */
-    public function index()
+
+    public function buscarCorrentista(Request $request, $email)
     {
-        /*
-        $correntista = [
-            'nome' => 'Poliana da Silva',
-            'email' => 'poli@gmail.com',
-            'username' => 'poli.silva',
-            'password' => Hash::make('220132')
-        ];
-        echo Hash::make('220132');exit;
-        Correntista::create($correntista);
-        */
-        return Correntista::all();
+        
+        return Correntista::where('email', '=', $email)->with('contaCorrente')->firstOrFail();
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,12 +2,14 @@
 /*
     Arquivo para tratamento dos endpoints
 */
-
-Route::get('transacoes', "TransacoesController@index");
-Route::get('correntistas', "CorrentistaController@index");
-// Buscar a conta corrente com o número da conta e agência
-Route::get('contacorrente/saldo/{numeroConta}/{agencia}', "ContaCorrenteController@buscarSaldoContaCorrente");
-// Realiza deposíto
-Route::post('contacorrente/depositar/{numeroConta}/{agencia}', "ContaCorrenteController@depositar");
-// Realizar saque
-Route::post('contacorrente/realizasaque/{numeroConta}/{agencia}', "ContaCorrenteController@realizasaque");
+Route::group(['middleware' => ['auth:api']], function() {
+    Route::get('transacoes', "TransacoesController@index")->middleware('auth:api');
+    // Buscar correntista, informações de conta e agência por URL
+    Route::get('correntista/{email}', "CorrentistaController@buscarCorrentista")->middleware('auth:api');
+    // Buscar a conta corrente com o número da conta e agência
+    Route::get('contacorrente/saldo/{numeroConta}/{agencia}', "ContaCorrenteController@buscarSaldoContaCorrente")->middleware('auth:api');
+    // Realiza deposíto
+    Route::post('contacorrente/depositar/{numeroConta}/{agencia}', "ContaCorrenteController@depositar")->middleware('auth:api');
+    // Realizar saque
+    Route::post('contacorrente/realizasaque/{numeroConta}/{agencia}', "ContaCorrenteController@realizasaque")->middleware('auth:api');
+});
